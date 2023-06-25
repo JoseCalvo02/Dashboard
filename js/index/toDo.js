a3a96a96dbedd2f7838032e910ae5aa173842616
JoseCalvo02 <joseantoniocalvoarias@gmail.com>
2023-06-24 23:12:30 -0600

    Working on todo and correcting color paletts

--- js/index/toDo.js
+++ js/index/toDo.js
@@ -16,6 +16,10 @@ function removeItem() {
 function addCloseEvent(element) {
     const closeBtn = element.querySelector('.close .material-icons-sharp');
     closeBtn.addEventListener('click', removeItem);
+
+    // Agregar el evento de clic al icono de completado en el nuevo elemento de tarea
+    const icon = element.querySelector('.icon .material-icons-sharp');
+    icon.addEventListener('click', toggleTaskCompletion);
 }
 
 // Agregar evento al span con el id "addToDoBtn"
@@ -24,17 +28,22 @@ addToDoBtn.addEventListener('click', function () {
     const input = document.querySelector('#taskInput');
     if (input) {
         const taskText = input.value.trim();
-        if (taskText.length > 0 && taskText.length <= 25) {
+        if (taskText.length > 0 && taskText.length <= 23) {
             addTask(taskText);
             input.value = '';
             input.parentNode.replaceChild(addTaskHeading, input);
+            input.blur(); // Quitar el enfoque del campo de entrada
         } else {
-            alert('El texto de la tarea debe tener entre 1 y 25 caracteres.');
+            alert('El texto de la tarea debe tener entre 1 y 23 caracteres.');
         }
     } else {
         convertToInput(addTaskHeading);
     }
-    input.focus(); // Agregar el enfoque al input después de la operación
+    const newInput = document.querySelector('#taskInput');
+    if (newInput) {
+        newInput.focus(); // Agregar el enfoque al nuevo input después de la operación
+        newInput.select(); // Seleccionar el texto del nuevo input
+    }
 });
 
 // Agregar evento al h3 con el id "addTaskHeading"
@@ -46,6 +55,28 @@ addTaskHeading.addEventListener('click', function () {
     input.select(); // Seleccionar el texto del input
 });
 
+function toggleTaskCompletion() {
+    const item = this.closest('.item');
+    const isCompleted = item.getAttribute('data-completed') === 'true';
+
+    item.setAttribute('data-completed', !isCompleted);
+
+    const icon = item.querySelector('.icon .material-icons-sharp');
+    const taskText = item.querySelector('.info h3'); // Obtener referencia al elemento <h3>
+
+    if (isCompleted) {
+        icon.textContent = 'close';
+        icon.style.color = 'var(--color-white)';
+        icon.parentElement.style.backgroundColor = 'var(--color-danger)';
+        taskText.style.textDecoration = 'none'; // Quitar línea de tachado
+    } else {
+        icon.textContent = 'check';
+        icon.style.color = 'var(--color-white)';
+        icon.parentElement.style.backgroundColor = 'var(--color-success)';
+        taskText.style.textDecoration = 'line-through'; // Aplicar línea de tachado
+    }
+}
+
 // Función para agregar una tarea a la lista
 function addTask(taskText) {
     if (!taskText) {
@@ -56,7 +87,12 @@ function addTask(taskText) {
         return;
     }
 
-    createTaskElement(taskText);
+    const divItem = createTaskElement(taskText);
+    divItem.setAttribute('data-completed', 'false');
+
+    // Agregar el evento de clic al icono de la nueva tarea
+    const icon = divItem.querySelector('.icon .material-icons-sharp');
+    icon.addEventListener('click', toggleTaskCompletion);
 }
 
 // Función para convertir un elemento en un campo de entrada
@@ -65,42 +101,85 @@ function convertToInput(element) {
     input.type = 'text';
     input.value = element.textContent.trim();
     input.id = 'taskInput';
-    input.maxLength = 25; // Establecer el límite máximo de caracteres
-    input.style.color = '#6380ec'; // Establecer el color del texto
+    input.maxLength = 23; // Establecer el límite máximo de caracteres
+    input.style.color = 'var(--color-primary)'; // Establecer el color del texto
     input.style.fontWeight = 'bold'; // Establecer el estilo del texto
+
+    const addTaskRow = document.querySelector('.add-task');
+    const tableRow = addTaskRow.closest('tr');
+    const tableBody = tableRow.parentNode;
+
     input.addEventListener('keydown', function (event) {
         if (event.key === 'Enter') {
+            event.preventDefault(); // Evitar el envío del formulario al presionar Enter
             const taskText = input.value.trim();
-            if (taskText.length > 0 && taskText.length <= 25) {
+            if (taskText.length > 0 && taskText.length <= 23) {
                 addTask(taskText);
                 input.parentNode.replaceChild(addTaskHeading, input);
+                input.blur(); // Quitar el enfoque del campo de entrada después del reemplazo
             } else {
-                alert('El texto de la tarea debe tener entre 1 y 25 caracteres.');
+                alert('El texto de la tarea debe tener entre 1 y 23 caracteres.');
             }
         } else if (event.key === 'Escape') {
             input.parentNode.replaceChild(addTaskHeading, input);
         }
     });
+
+    input.addEventListener('blur', function () {
+        setTimeout(function () {
+            const taskInput = document.querySelector('#taskInput');
+            if (taskInput && document.activeElement !== taskInput) {
+                taskInput.parentNode.replaceChild(addTaskHeading, taskInput);
+            }
+        }, 100);
+    });
+
     element.parentNode.replaceChild(input, element);
     input.focus();
     input.select(); // Seleccionar el texto del input
+
+    // Evento clic en el documento para cerrar el campo de entrada si se hace clic fuera de él
+    document.addEventListener('click', function (event) {
+        const taskInput = document.querySelector('#taskInput');
+        if (taskInput && event.target !== taskInput && !taskInput.contains(event.target)) {
+            setTimeout(function () {
+                if (document.activeElement !== taskInput) {
+                    taskInput.parentNode.replaceChild(addTaskHeading, taskInput);
+                }
+            }, 100);
+        }
+    });
+
+    // Evento clic en la fila de agregar tarea para cerrar el campo de entrada si se hace clic en ella
+    addTaskRow.addEventListener('click', function (event) {
+        const taskInput = document.querySelector('#taskInput');
+        if (taskInput && event.target === addTaskRow) {
+            taskInput.parentNode.replaceChild(addTaskHeading, taskInput);
+        }
+    });
 }
+
+let taskId = 0; // Variable para asignar ID único a cada tarea
 
 // Función para crear un nuevo elemento de tarea
 function createTaskElement(taskText) {
+    // Incrementar el ID único para la siguiente tarea
+    taskId++;
+
     const divItem = document.createElement('div');
     divItem.className = 'item online';
+    divItem.setAttribute('data-completed', 'false');
+
+    divItem.dataset.taskId = taskId; // Asignar el ID único a través del atributo dataset
 
     const itemContent = `
         <div class="icon">
-            <span class="material-icons-sharp">more_horiz</span>
+            <span class="material-icons-sharp">close</span>
         </div>
         <div class="right">
             <div class="info">
                 <h3>${taskText}</h3>
-                <small class="text-muted">Last 24 Hours</small>
             </div>
-            <h5 class="success">+39%</h5>
             <div class="close" id="close-btn">
                 <span class="material-icons-sharp">close</span>
             </div>
