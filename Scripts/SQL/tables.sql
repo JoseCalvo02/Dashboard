6bb206efadabe419c8292746bc66dca3f37552a3
JoseCalvo02 <joseantoniocalvoarias@gmail.com>
2023-07-10 23:46:35 -0600

    Working on the register form

--- Scripts/SQL/tables.sql
+++ Scripts/SQL/tables.sql
@@ -1,18 +1,20 @@
 use TaskHub_DB;
 
+drop table registro_usuario;
+
 -- Tabla de registro de usuarios
 CREATE TABLE registro_usuario (
-    id_usuario INT IDENTITY(1,1) PRIMARY KEY,
-    nombre_usuario VARCHAR(100) NOT NULL,
-    correo_usuario VARCHAR(100) NOT NULL UNIQUE,
-    contraseña_usuario VARCHAR(100) NOT NULL
+    id_user INT IDENTITY(1,1) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    correo VARCHAR(100) NOT NULL UNIQUE,
+    contraseña VARCHAR(100) NOT NULL
 );
 
 -- Tabla de registro de proyectos
 CREATE TABLE proyectos (
-    id_proyecto INT IDENTITY(1,1) PRIMARY KEY,
-    nombre_proyecto VARCHAR(100) NOT NULL,
-    descripcion_proyecto VARCHAR(100) NOT NULL,
-    fecha_inicio_proyecto DATE DEFAULT GETDATE(),
-    fecha_vencimiento_proyecto DATE
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    descripcion VARCHAR(100) NOT NULL,
+    fecha_inicio DATE DEFAULT GETDATE(),
+    fecha_vencimiento DATE
 );
