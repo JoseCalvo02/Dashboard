3de788ec66bbd6814cc8df396779303ea4e46270
JoseCalvo02 <joseantoniocalvoarias@gmail.com>
2023-07-10 23:46:09 -0600

    Working on the register form

--- Models/User/registerUserModel.js
+++ Models/User/registerUserModel.js
@@ -1,20 +1,19 @@
-module.exports = {
-    registerUser,
-};
+const sql = require('mssql');
+const dbConfig = require('../Server/dbConfig');
 
-async function registerUser(nombre, correo, contraseña) {
+async function registerUser(name, email, password) {
     try {
         // Conectar a la base de datos
         const pool = await sql.connect(dbConfig);
 
         // Ejecutar la consulta de inserción
-        const query = `INSERT INTO registro_usuario (nombre, correo, contraseña) VALUES (@nombre, @correo, @contraseña)`;
+        const query = `INSERT INTO registro_usuario (name, email, password) VALUES (@nombre, @correo, @contraseña)`;
 
         // Ejecutar consultas o operaciones en la base de datos
         const request = pool.request();
-        request.input('nombre', sql.VarChar(100), nombre);
-        request.input('correo', sql.VarChar(100), correo);
-        request.input('contraseña', sql.VarChar(100), contraseña);
+        request.input('nombre', sql.VarChar(100), name);
+        request.input('correo', sql.VarChar(100), email);
+        request.input('contraseña', sql.VarChar(100), password);
         const result = await request.query(query);
 
         console.log('Usuario registrado exitosamente');
@@ -22,3 +21,7 @@ async function registerUser(nombre, correo, contraseña) {
         console.error('Error al registrar el usuario:', error);
     }
 }
+
+module.exports = {
+    registerUser,
+};
