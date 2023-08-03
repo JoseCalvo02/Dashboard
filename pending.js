80bca7c502127921202014862661947044c9f597
JoseCalvo02 <joseantoniocalvoarias@gmail.com>
2023-08-02 22:11:50 -0600

    Adding  pending tasks for the project

--- pending.js
+++ pending.js
@@ -1,5 +1,11 @@
 /*
+!URGENT
+TODO: Add the edit text functionality to the reminders
+TODO: Add the delete functionality to the reminders
+
+?PENDING CORRECTIONS
 TODO: Validate that passwords are identical including uppercase & lowercase letters
 TODO: Cerrar elemento al volver a dar al icono de los reminders
 TODO: lograr recargar el index al crear un proyecto nuevo
+TODO: Correct the way the deadline is saved when creating a project
 */
